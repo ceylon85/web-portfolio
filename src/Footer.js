979592dc873f1b7a437adbf9a31d3a9d32a66591
ceylon85 <canleez123@gmail.com>
2021-05-03 17:48:20 +0900

    Portfolio - footer

--- src/Footer.js
+++ src/Footer.js
@@ -7,9 +7,14 @@ function Footer() {
   return (
     <footer>
       <div className="footer__container">
-        <div id="go-top"className="footer__top" >
+        <div id="go-top" className="footer__top">
           <a className="footer__BacktoTop" title="Back to Top" href="#home">
-            <FontAwesomeIcon icon={faAngleDoubleUp} color="gray" size="lg" className="fupIcon--active" />
+            <FontAwesomeIcon
+              icon={faAngleDoubleUp}
+              color="gray"
+              size="lg"
+              className="fupIcon--active"
+            />
           </a>
         </div>
         <div className="footer__row1">
@@ -20,13 +25,18 @@ function Footer() {
         </div>
         <div className="footer__row2">
           <span>Copyright &copy; 2021</span>
-          <a href="https://github.com/ceylon85/react-amazon-clone">
-            <img
-              className="git__logo"
-              src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-              alt="git"
-              title="클릭하시면 github로 이동합니다"
-            />
+          <a
+            className="git__logo"
+            href="https://github.com/ceylon85/react-amazon-clone"
+          >
+            <span>
+              <img
+                className="git__logoImg"
+                src="https://www.flaticon.com/svg/vstatic/svg/889/889111.svg?token=exp=1620023235~hmac=4a5d8df4a66da9a10036ae4805ecb9bd"
+                alt="git"
+                title="클릭하시면 github로 이동합니다"
+              />
+            </span>
           </a>
         </div>
       </div>
