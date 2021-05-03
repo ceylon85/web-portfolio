c32c9b6dd5ef1e334b16a7e998143542d24d2824
ceylon85 <canleez123@gmail.com>
2021-05-03 18:26:19 +0900

    Portfolio - contact

--- src/Contact.js
+++ src/Contact.js
@@ -1,80 +1,168 @@
-import React from "react";
+import React, { useState } from "react";
+import { Zoom } from "react-reveal";
+import ReactLoading from "react-loading";
+import emailjs from "emailjs-com";
 
-function Contact() {
+import "./css/Contact.css";
+
+const Contact = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [messageSent, setMessageSent] = useState(false);
+  const [userName, setUserName] = useState();
+  const [userEmail, setUserEmail] = useState();
+  const [userMessage, setUserMessage] = useState();
+  const [missingName, setMissingName] = useState(false);
+  const [missingEmail, setMissingEmail] = useState(false);
+  const [missingMessage, setMissingMessage] = useState(false);
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    if (!userName || userName === "") {
+      setMissingName(true);
+    }
+    if (!userEmail || userEmail === "") {
+      setMissingEmail(true);
+    }
+    if (!userMessage || userMessage === "") {
+      setMissingMessage(true);
+    }
+    if (
+      userName &&
+      userName !== "" &&
+      userEmail &&
+      userEmail !== "" &&
+      userMessage &&
+      userMessage !== ""
+    ) {
+      setIsLoading(true);
+      emailjs
+        .sendForm("gmail", "portfolio", e.target, "user_uAVob3QYnNs3pPIR9EHKG")
+        .then(
+          () => {
+            setMessageSent(true);
+          },
+          (error) => {
+            console.log(error.text);
+            window.alert(
+              "메시지를 보내는 중에 문제가 발생했습니다. 다시 시도하십시오!!"
+            );
+          }
+        )
+        .then(() => {
+          setMessageSent(true);
+          setIsLoading(false);
+          setUserName();
+          setUserEmail();
+          setUserMessage();
+        });
+    }
+  };
   return (
-    <div>
-      연락처 Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Praesentium nemo, repellat quos deserunt sunt nesciunt accusantium
-      dignissimos aliquid ut porro quod, maxime quam sint natus distinctio
-      eveniet dolorem. Quidem, eaque!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Praesentium nemo, repellat quos deserunt sunt nesciunt accusantium
-      dignissimos aliquid ut porro quod, maxime quam sint natus distinctio
-      eveniet dolorem. Quidem, eaque!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Praesentium nemo, repellat quos deserunt sunt nesciunt accusantium
-      dignissimos aliquid ut porro quod, maxime quam sint natus distinctio
-      eveniet dolorem. Quidem, eaque!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Praesentium nemo, repellat quos deserunt sunt nesciunt accusantium
-      dignissimos aliquid ut porro quod, maxime quam sint natus distinctio
-      eveniet dolorem. Quidem, eaque!
-      v
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Praesentium nemo, repellat quos deserunt sunt nesciunt accusantium
-      dignissimos aliquid ut porro quod, maxime quam sint natus distinctio
-      eveniet dolorem. Quidem, eaque!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Praesentium nemo, repellat quos deserunt sunt nesciunt accusantium
-      dignissimos aliquid ut porro quod, maxime quam sint natus distinctio
-      eveniet dolorem. Quidem, eaque!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Praesentium nemo, repellat quos deserunt sunt nesciunt accusantium
-      dignissimos aliquid ut porro quod, maxime quam sint natus distinctio
-      eveniet dolorem. Quidem, eaque!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Praesentium nemo, repellat quos deserunt sunt nesciunt accusantium
-      dignissimos aliquid ut porro quod, maxime quam sint natus distinctio
-      eveniet dolorem. Quidem, eaque!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Praesentium nemo, repellat quos deserunt sunt nesciunt accusantium
-      dignissimos aliquid ut porro quod, maxime quam sint natus distinctio
-      eveniet dolorem. Quidem, eaque!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Praesentium nemo, repellat quos deserunt sunt nesciunt accusantium
-      dignissimos aliquid ut porro quod, maxime quam sint natus distinctio
-      eveniet dolorem. Quidem, eaque!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Praesentium nemo, repellat quos deserunt sunt nesciunt accusantium
-      dignissimos aliquid ut porro quod, maxime quam sint natus distinctio
-      eveniet dolorem. Quidem, eaque!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Praesentium nemo, repellat quos deserunt sunt nesciunt accusantium
-      dignissimos aliquid ut porro quod, maxime quam sint natus distinctio
-      eveniet dolorem. Quidem, eaque!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Praesentium nemo, repellat quos deserunt sunt nesciunt accusantium
-      dignissimos aliquid ut porro quod, maxime quam sint natus distinctio
-      eveniet dolorem. Quidem, eaque!
-      <var>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Praesentium nemo, repellat quos deserunt sunt nesciunt accusantium
-      dignissimos aliquid ut porro quod, maxime quam sint natus distinctio
-      eveniet dolorem. Quidem, eaque!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Praesentium nemo, repellat quos deserunt sunt nesciunt accusantium
-      dignissimos aliquid ut porro quod, maxime quam sint natus distinctio
-      eveniet dolorem. Quidem, eaque!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Praesentium nemo, repellat quos deserunt sunt nesciunt accusantium
-      dignissimos aliquid ut porro quod, maxime quam sint natus distinctio
-      eveniet dolorem. Quidem, eaque!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Praesentium nemo, repellat quos deserunt sunt nesciunt accusantium
-      dignissimos aliquid ut porro quod, maxime quam sint natus distinctio
-      eveniet dolorem. Quidem, eaque!
-      v</var>
+    <div id="contactSection" className="section contact-section">
+      <div className="title contact-title">
+        {!messageSent ? (
+          <Zoom>
+            <h1>Let's talk!</h1>
+          </Zoom>
+        ) : (
+          <h1>메세지가 성공적으로 전송되었습니다!!</h1>
+        )}
+        {!messageSent ? (
+          <Zoom>
+            <h3>
+            아래 양식을 작성하거나 다음의 이메일로 연락바랍니다.{" "}
+              <a href="canleez123@gmail.com">
+                canleez123@gmail.com
+              </a>{" "}
+            </h3>
+          </Zoom>
+        ) : (
+          <>
+            <h3>감사합니다! 빠른 시간 안에 회신하겠습니다.!</h3>
+            <p
+              className="send-another-message"
+              onClick={() => setMessageSent(false)}
+            >
+              다른 메세지를 보내시겠습니까?
+            </p>
+          </>
+        )}
+      </div>
+      {!messageSent && (
+        <div className="contact-form__container">
+          <Zoom>
+            <form className="contact-form" onSubmit={(e) => formSubmit(e)}>
+              <div className="name-email-input__container">
+                <div className="name-input__container">
+                  <input
+                    className={missingName && "missing-input"}
+                    type="text"
+                    placeholder="Your name"
+                    name="user_name"
+                    onChange={(e) => {
+                      setUserName(e.target.value);
+                      setMissingName(false);
+                    }}
+                  />
+                  {missingName && (
+                    <p className="required-input-message">
+                      이름을 작성해주세요!
+                    </p>
+                  )}
+                </div>
+                <div className="email-input__container">
+                  <input
+                    className={missingEmail && "missing-input"}
+                    type="email"
+                    placeholder="Your@email.com"
+                    name="user_email"
+                    onChange={(e) => {
+                      setUserEmail(e.target.value);
+                      setMissingEmail(false);
+                    }}
+                  />
+                  {missingEmail && (
+                    <p className="required-input-message">
+                      Email을 작성해주세요!!
+                    </p>
+                  )}
+                </div>
+              </div>
+              <div className="message-input__container">
+                <textarea
+                  className={missingMessage && "missing-textarea"}
+                  placeholder="Your message"
+                  name="user_message"
+                  onChange={(e) => {
+                    setUserMessage(e.target.value);
+                    setMissingMessage(false);
+                  }}
+                />
+                {missingMessage && (
+                  <p className="required-input-message">
+                    메세지를 작성해주세요!!
+                  </p>
+                )}
+              </div>
+              <div className="send-btn__container">
+                {isLoading && (
+                  <div className="loading-spinner__container">
+                    <ReactLoading
+                      type={"spin"}
+                      color={"#fff"}
+                      height={40}
+                      width={40}
+                    />
+                  </div>
+                )}
+                {!isLoading && <button className="send-btn">Send</button>}
+              </div>
+            </form>
+          </Zoom>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default Contact;
